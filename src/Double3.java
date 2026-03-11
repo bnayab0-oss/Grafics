fa87d66f4447b2f01b92e23e9f8ee5c2b71ea424
bnaya <bnaya.b.0@gmail.com>
2026-03-11 12:09:04 +0200

    ניסיון

--- src/Double3.java
+++ src/Double3.java
@@ -6,7 +6,6 @@ public record Double3 (double _d1, double _d2, double _d3) {
 
     @Override
     public String toString() { return "(" + _d1 + "," + _d2 + "," + _d3 + ")" ; }
-
     @Override
     public boolean equals(Object obj) {
         return this == obj || (obj instanceof Double3(double od1, double od2, double od3))// uses Util.isZero()
@@ -18,4 +17,6 @@ public record Double3 (double _d1, double _d2, double _d3) {
     @Override
     public int hashCode() { return Objects.hash(_d1, _d2, _d3); }
 // … Operations …
+
+    bnaya חחחחח
 }
